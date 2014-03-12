0cc9b7e216a1a5028155c466c8c745f275b92916
sirguy <guylaingreer@gmail.com>
2014-03-11 21:44:55 -0400

    Adding some space-eating capability

--- comment.cpp
+++ comment.cpp
@@ -1,13 +1,15 @@
 #include "comment.hh"
 #include "common.hh"
 #include <boost/spirit/include/qi.hpp>
+#include <boost/spirit/include/phoenix.hpp>
 
 using namespace boost::spirit;
+using namespace boost::phoenix;
 
 qi::rule<common::iter, Comment()> Comment::get_rule()
 {
     static qi::rule<common::iter, Comment()> r =
-        "!" >> *(qi::char_ - common::newline);
+        "!" >> *qi::blank >> *(qi::char_[at_c<0>(_val) += _1] - common::newline);
     r.name("Comment");
 #ifdef ENABLE_SPIRIT_DEBUGGING
     debug(r);
